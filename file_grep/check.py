67b2b902f24496c781970c7a72f84cf9d588e324
Jeffrey Hulten <jeffrey@moz.com>
2017-10-04 11:14:48 -0700

    Search for match in mmap file

--- file_grep/check.py
+++ file_grep/check.py
@@ -3,16 +3,68 @@
 # Licensed under Simplified BSD License (see LICENSE)
 
 # stdlib
+import mmap
+import os
 
 # 3rd party
 
 # project
 from checks import AgentCheck
 
-EVENT_TYPE = SOURCE_TYPE_NAME = 'file_grep'
+class FileGrepCheck(AgentCheck):
 
-
-class File_grepCheck(AgentCheck):
+    SOURCE_TYPE_NAME = 'file_grep'
+    SERVICE_CHECK_NAME = 'file_grep.match_ok'
 
     def check(self, instance):
-        pass
+        self.log.debug('starting check run')
+
+        check_name = instance.get('name')
+        filename = instance.get('file')
+        search_str = instance.get('search_string')
+
+        result = None
+        service_check_tags = []
+        service_check_tags.append('check_name:{}'.format(check_name))
+
+        try:
+            with ctx_fd(filename) as fd:
+                with ctx_mmap(fd) as mm:
+                    result = mm.find(search_str)
+        except Exception as e:
+            self.service_check(
+                self.SERVICE_CHECK_NAME, AgentCheck.UNKNOWN, tags=service_check_tags)
+            self.warning('Error({}): {}'.format(type(e).__name__ , e))
+            return
+
+        if result == -1:
+            # NO MATCH
+            self.log.debug('NO MATCH')
+            self.service_check(
+                self.SERVICE_CHECK_NAME, AgentCheck.CRITICAL, tags=service_check_tags)
+        else:
+            self.log.debug('MATCH')
+            self.service_check(
+                self.SERVICE_CHECK_NAME, AgentCheck.OK, tags=service_check_tags)
+
+class ctx_fd():
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __enter__(self):
+        self.fd = os.open(self.filename, os.O_RDONLY)
+        return self.fd
+
+    def __exit__(self, *args):
+        os.close(self.fd)
+
+class ctx_mmap():
+    def __init__(self, fd):
+        self.fd = fd
+
+    def __enter__(self):
+        self.mm = mmap.mmap(self.fd, 0, prot=mmap.PROT_READ)
+        return self.mm
+
+    def __exit__(self, *args):
+        self.mm.close()
--- file_grep/test_file_grep.py
+++ file_grep/test_file_grep.py
@@ -4,35 +4,61 @@
 
 # stdlib
 from nose.plugins.attrib import attr
+import os
 
 # 3p
 
 # project
 from tests.checks.common import AgentCheckTest
 
+CI_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "ci")
 
-instance = {
-    'host': 'localhost',
-    'port': 26379,
-    'password': 'datadog-is-devops-best-friend'
+
+def merge_dicts(x, y):
+    z = x.copy()
+    z.update(y)
+    return z
+
+MOCK_CONFIG = {
+    'init_config': {},
+    'instances': [{
+        'name': 'test_success',
+        'file': "{}/{}".format(CI_DIR, 'pass.txt'),
+        'search_string': 'Success'
+    }, {
+        'name': 'test_failure',
+        'file': "{}/{}".format(CI_DIR, 'fail.txt'),
+        'search_string': 'CannotSuccess'
+    }, {
+        'name': 'test_missing',
+        'file': "{}/{}".format(CI_DIR, 'notfound.txt'),
+        'search_string': 'Success'
+    }
+    ]
 }
 
+MOCK_AGENT_CONFIG = merge_dicts(AgentCheckTest.DEFAULT_AGENT_CONFIG, {
+    'hostname': 'test.local'
+})
 
-# NOTE: Feel free to declare multiple test classes if needed
 
 @attr(requires='file_grep')
-class TestFile_grep(AgentCheckTest):
+class TestFileGrep(AgentCheckTest):
     """Basic Test for file_grep integration."""
     CHECK_NAME = 'file_grep'
 
     def test_check(self):
         """
-        Testing File_grep check.
+        Testing FileGrep check.
         """
-        self.load_check({}, {})
+        self.run_check(MOCK_CONFIG, MOCK_AGENT_CONFIG)
 
-        # run your actual tests...
+        self.assertServiceCheckOK('file_grep.match_ok',
+                                  tags=['check_name:test_success'])
+        self.assertServiceCheckCritical('file_grep.match_ok',
+                                  tags=['check_name:test_failure'])
+        self.assertServiceCheckUnknown('file_grep.match_ok',
+                                  tags=['check_name:test_missing'])
 
-        self.assertTrue(True)
         # Raises when COVERAGE=true and coverage < 100%
         self.coverage_report()
