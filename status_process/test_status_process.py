09c9a56232e17c487d25fde5def1a977408eb001
Jeffrey Hulten <jeffrey@moz.com>
2017-10-06 13:47:35 -0700

    add tests

--- status_process/test_status_process.py
+++ status_process/test_status_process.py
@@ -10,29 +10,48 @@ from nose.plugins.attrib import attr
 # project
 from tests.checks.common import AgentCheckTest
 
+def merge_dicts(x, y):
+    z = x.copy()
+    z.update(y)
+    return z
 
-instance = {
-    'host': 'localhost',
-    'port': 26379,
-    'password': 'datadog-is-devops-best-friend'
+MOCK_CONFIG = {
+    'init_config': {},
+    'instances' : [{
+        'check_name': 'true.passes',
+        'command': '/bin/true'
+    }, {
+        'check_name': 'false.fails',
+        'command': '/bin/false'
+    }, {
+        'check_name': 'false.passes.with.rc1',
+        'command': '/bin/false',
+        'return_codes': [0, 1]
+    }]
 }
+
+MOCK_AGENT_CONFIG = merge_dicts(AgentCheckTest.DEFAULT_AGENT_CONFIG, {
+    'hostname': 'test.local'
+})
 
 
 # NOTE: Feel free to declare multiple test classes if needed
 
 @attr(requires='status_process')
-class TestStatus_process(AgentCheckTest):
+class TestStatusProcess(AgentCheckTest):
     """Basic Test for status_process integration."""
     CHECK_NAME = 'status_process'
 
     def test_check(self):
         """
-        Testing Status_process check.
+        Testing StatusProcess check.
         """
-        self.load_check({}, {})
+        self.run_check(MOCK_CONFIG, MOCK_AGENT_CONFIG)
+        self.assertServiceCheckOK('status_process.ok',
+                                  tags=['check_name:true.passes'])
+        self.assertServiceCheckCritical('status_process.ok',
+                                  tags=['check_name:false.fails'])
+        self.assertServiceCheckOK('status_process.ok',
+                                  tags=['check_name:false.passes.with.rc1'])
 
-        # run your actual tests...
-
-        self.assertTrue(True)
-        # Raises when COVERAGE=true and coverage < 100%
         self.coverage_report()
